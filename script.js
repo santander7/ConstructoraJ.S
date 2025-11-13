29dde0b1b362c2ad2466afa76e3b76e3feb6d1bc
santander7 <jhonsantander2011@gmail.com>
2025-11-13 09:05:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,22 @@ function abrirGaleria(proyecto) {
 function cerrarGaleria() {
   document.getElementById("modalGaleria").style.display = "none";
 }
+const images = document.querySelectorAll('.proyecto img');
+const lightbox = document.createElement('div');
+lightbox.classList.add('lightbox');
+document.body.appendChild(lightbox);
+
+const img = document.createElement('img');
+lightbox.appendChild(img);
+
+images.forEach(image => {
+  image.addEventListener('click', () => {
+    img.src = image.src;
+    lightbox.classList.add('active');
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.classList.remove('active');
+});
+
